373614d4c27e1aab1866109bec9d1c7511286e73
Nathan Frasier <nanoshot.nf@gmail.com>
2020-06-12 15:46:29 -0500

    Create cmd line framework

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+use std::path::PathBuf;
+use structopt::clap::arg_enum;
+use structopt::StructOpt;
+
+arg_enum! {
+    #[derive(PartialEq, Debug)]
+    enum Solver {
+        Basic
+    }
+}
+
+#[derive(Debug, StructOpt)]
+struct Opt {
+  problems: PathBuf,
+
+  #[structopt(possible_values = &Solver::variants(), case_insensitive = true)]
+  #[structopt(min_values = 1)]
+  #[structopt(short, long)]
+  solvers: Vec<Solver>
+}
+
 fn main() {
     println!("Hello, world!");
+    let mut opt = Opt::from_args();
+    opt.solvers.dedup();
+    println!("{:?}", opt);
 }
