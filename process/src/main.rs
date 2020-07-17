cd1123ca38cd5268056986b01ed4782374897192
aecium <aecium@gmail.com>
2020-07-17 15:01:46 -0500

    added symbol emun and function stubs

--- process/src/main.rs
+++ process/src/main.rs
@@ -3,6 +3,194 @@ use std::io::{self, BufRead};
 use std::path::Path;
 use std::env;
 
+enum Symbol {
+    Equality,
+    Successor,
+    Predecessor,
+    Sum,
+    Product,
+    IntegerDivision,
+    StrictLessThan,
+    Modulate,
+    Demodulate,
+    SendNow, //Send
+    Negate,
+    FuncApp, //Function Application
+    SC, //S Combinator
+    CC, //C Combinator
+    BC, //B Combinator
+    TC, // True (K Combinator)
+    FC, // False
+    PowerOfTwo,
+    IC, //i Combinator
+    Cons, //(or Pair)
+    Car, //(First)
+    Cdr, //(Tail)
+    Nil, //(Empty List)
+    IsNil, //(Is Empty List)
+    LCS, //List Construction Syntax
+    Vector,
+    Draw,
+    Checkerboard,
+    MulDraw, //Multiple Draw
+    ModList, //Modulate List
+    Send0,
+    Is0,
+    Interact,
+    InterProt, //Interaction Protocol
+    StatesessDraw, //Stateless Drawing Protocol
+    StatefullDraw, //Statefull Drawing Protocol
+    Galaxy,
+}
+
+fn equality(x: i32, y: i32) -> bool {
+    true
+}
+
+fn successor(x: i32) -> i32{
+    0
+}
+
+fn predecessor(x: i32) -> i32 {
+    0
+}
+
+fn sum(x: i32, y: i32) -> i32 {
+    0
+}
+
+fn product(x: i32, y: i32) -> i32  {
+    0
+}
+
+fn integer_division(x: i32, y: i32) -> i32  {
+    0
+}
+
+fn strictless_than(x: i32, y: i32) -> bool  {
+    true
+}
+
+fn modulate(x: i32) {
+
+}
+
+fn demodulate(s: String) -> i32 {
+    0
+}
+
+fn send_now(s: &str) {
+
+}
+
+fn negate(x: i32) -> i32 {
+    0
+}
+
+fn func_app() { //Function Application
+
+}
+
+fn sc() { //S Combinator
+
+}
+
+fn cc(){ //C Combinator
+
+}
+
+fn bc() { //B Combinator
+
+}
+
+fn tcom() { // True (K Combinator)
+
+}
+
+fn fcom() { // False
+
+}
+
+fn power_of_two() {
+
+}
+
+fn ic() { //i Combinator
+
+}
+
+fn cons() { //(or Pair)
+
+}
+
+fn car() { //(First)
+
+}
+
+fn cdr() { //(Tail)
+
+}
+
+fn nil() { //(Empty List)
+
+}
+
+fn is_nil() { //(Is Empty List)
+
+}
+
+fn lcs() { //List Construction Syntax
+
+}
+
+fn vector() {
+
+}
+
+fn draw() {
+
+}
+
+fn checkerboard() {
+
+}
+
+fn mul_draw() { //Multiple Draw
+
+}
+
+fn mod_list() { //Modulate List
+
+}
+
+fn send0() {
+
+}
+
+fn is0() {
+
+}
+
+fn interact() {
+
+}
+
+fn inter_prot() { //Interaction Protocol
+
+}
+
+fn statesess_draw() { //Stateless Drawing Protocol
+
+}
+
+fn statefull_draw() { //Statefull Drawing Protocol
+
+}
+
+fn galaxy() {
+
+}
+
 fn main() {
     // File hosts must exist in current path before this produces output
     let args: Vec<String> = env::args().collect();
@@ -17,6 +205,8 @@ fn main() {
         }
     }
 }
+
+
 
 // The output is wrapped in a Result to allow matching on errors
 // Returns an Iterator to the Reader of the lines of the file.
