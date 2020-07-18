4c4587d9d5e5dd43908074d8faa41b5437fe7309
Nathan Frasier <nanoshot.nf@gmail.com>
2020-07-18 02:11:15 -0500

    Implmenent most of the parser combinator logic needed

--- galaxy_parse/src/galaxy_parse.rs
+++ galaxy_parse/src/galaxy_parse.rs
@@ -18,54 +18,58 @@ pub enum Ops {
     Nil,
 }
 
-impl Ops {
-    fn named_parse(input: &str) -> Self {
-        match input {
-            _ => Self::Nil
-        }
-    }
-}
-
-fn take_token(input : &'_ str) -> IResult<&'_ str, &'_ str> {
-    nom::bytes::complete::is_not(" ")(input)
-}
-
 fn parse_literal(input : &'_ str) -> IResult<&'_ str, Ops> {
     nom::combinator::map_parser(nom::character::complete::digit1, |digits: &str| -> IResult<&'_ str, Ops>{
-        match input.parse::<i128>() {
-            Ok(num) => Ok((&input[input.len()..input.len()], Ops::Literal(num))),
+        match digits.parse::<i128>() {
+            Ok(num) => Ok((&input[digits.len()..input.len()], Ops::Literal(num))),
             _ => IResult::Err(nom::Err::Error(nom::error::make_error(input, nom::error::ErrorKind::IsNot)))
         }
     })(input)
 }
 
 fn parse_func(input : &'_ str) -> IResult<&'_ str, Ops> {
-    nom::combinator::map_parser(nom::character::complete::alpha1, |opName: &str| -> IResult<&'_ str, Ops>{
-        match opName {
-            "nil" => Ok((&input[input.len()..input.len()], Ops::Nil)),
-            "cons" => Ok((&input[input.len()..input.len()], Ops::Cons)),
-            "ap" => Ok((&input[input.len()..input.len()], Ops::Nil)), //TODO this is definitely wrong, time for recursion
-
-            _ => IResult::Err(nom::Err::Error(nom::error::make_error(input, nom::error::ErrorKind::IsNot)))
-        }
-    })(input)
+    nom::branch::alt((
+        //ap
+        nom::combinator::map(nom::sequence::tuple((
+            nom::bytes::complete::tag("ap"),
+            nom::sequence::preceded(nom::character::complete::space1, parse_symbol),
+            nom::sequence::preceded(nom::character::complete::space1, parse_symbol),
+        )), |(_, left, right)| Ops::Ap(Box::new(left), Box::new(right))),
+        //cons
+        nom::combinator::map(nom::bytes::complete::tag("cons"), |_| Ops::Cons),
+        //nil
+        nom::combinator::map(nom::bytes::complete::tag("nil"), |_| Ops::Nil)
+    ))(input)
 }
 
-pub fn parse_line(input : &'_ str) -> IResult<&'_ str, Ops> {
-    Ok((input, Ops::Nil))
+pub fn parse_symbol(input : &'_ str) -> IResult<&'_ str, Ops> {
+    nom::branch::alt((
+        parse_literal,
+        parse_func
+    ))(input)
+}
+
+pub fn _parse_line(input : &'_ str) -> IResult<&'_ str, Ops> {
+    //TODO handle the beginning of the line
+    parse_symbol(input)
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
     #[test]
-    fn test_take_token() {
-        assert_eq!(take_token("42 "),Ok((" ", "42")))
-    }
-    #[test]
     fn test_parse_literal() {
         let (remainder, op) = (parse_literal("42")).unwrap();
         assert_eq!(remainder, "");
+        match op {
+            Ops::Literal(42) => assert!(true),
+            _ => assert!(false)
+        };
+    }
+    #[test]
+    fn test_parse_literal_with_more() {
+        let (remainder, op) = (parse_literal("42 27")).unwrap();
+        assert_eq!(remainder, " 27");
         match op {
             Ops::Literal(42) => assert!(true),
             _ => assert!(false)
@@ -88,5 +92,49 @@ mod tests {
             Ops::Cons => assert!(true),
             _ => assert!(false)
         };
+    }
+    #[test]
+    fn test_parse_sym_1() {
+        let (remainder, op) = (parse_symbol("1")).unwrap();
+        assert_eq!(remainder, "");
+        match op {
+            Ops::Literal(1) => assert!(true),
+            _ => assert!(false)
+        };
+    }
+    #[test]
+    fn test_parse_sym_cons() {
+        let (remainder, op) = (parse_symbol("cons")).unwrap();
+        assert_eq!(remainder, "");
+        match op {
+            Ops::Cons => assert!(true),
+            _ => assert!(false)
+        };
+    }
+    #[test]
+    fn test_parse_sym_ap_1_2() {
+        let (remainder, op) = (parse_symbol("ap 1 2")).unwrap();
+        assert_eq!(remainder, "");
+        match op {
+            Ops::Ap(left, right) => {
+                if let left = Ops::Literal(1) {
+                    // correct
+                } else {
+                    assert!(false);
+                };
+                if let right = Ops::Literal(2) {
+                    // correct
+                } else {
+                    assert!(false);
+                }
+            },
+            _ => assert!(false)
+        };
+    }
+    #[test]
+    fn test_parse_sym_ap_ap_cons_7_nil() {
+        let (remainder, op) = (parse_symbol("ap ap cons 7 nil")).unwrap();
+        assert_eq!(remainder, "");
+        dbg!(op);
     }
 }
