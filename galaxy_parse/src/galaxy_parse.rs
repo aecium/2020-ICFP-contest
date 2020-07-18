a5484cab37235652a85b1885e9e58c5f0580384d
Nathan Frasier <nanoshot.nf@gmail.com>
2020-07-18 14:12:34 -0500

    Finish(?) the parser for galaxy.txt

--- galaxy_parse/src/galaxy_parse.rs
+++ galaxy_parse/src/galaxy_parse.rs
@@ -3,19 +3,33 @@ use nom::IResult;
 #[derive(Debug)]
 pub enum Ops {
     Literal(i128),
-    Bind(usize, Box<Ops>), // Assignment Equality / Bind(memloc, ops)
-    /*
+    Variable(String),
+    Ap(Box<Ops>, Box<Ops>),
+    List(Vec<Ops>),
+    Cons,
+    Car,
+    Cdr,
+    Nil,
+    IsNil,
     Inc,
     Dec,
     Sum,
     Mul,
     Div,
     Eq,
-    */
-    
-    Ap(Box<Ops>, Box<Ops>),
-    Cons,
-    Nil,
+    Lt,
+    SComb,
+    BComb,
+    CComb,
+    IComb,
+    TChoice,
+    FChoice,
+}
+
+impl Ops {
+    fn arity(&self) -> usize {
+        0
+    }
 }
 
 fn parse_literal(input : &'_ str) -> IResult<&'_ str, Ops> {
@@ -38,20 +52,85 @@ fn parse_func(input : &'_ str) -> IResult<&'_ str, Ops> {
         //cons
         nom::combinator::map(nom::bytes::complete::tag("cons"), |_| Ops::Cons),
         //nil
-        nom::combinator::map(nom::bytes::complete::tag("nil"), |_| Ops::Nil)
+        nom::combinator::map(nom::bytes::complete::tag("nil"), |_| Ops::Nil),
+        //inc
+        nom::combinator::map(nom::bytes::complete::tag("inc"), |_| Ops::Inc),
+        //dec
+        nom::combinator::map(nom::bytes::complete::tag("dec"), |_| Ops::Dec),
+        //sum
+        nom::combinator::map(nom::bytes::complete::tag("add"), |_| Ops::Sum),
+        //mul
+        nom::combinator::map(nom::bytes::complete::tag("mul"), |_| Ops::Mul),
+        //div
+        nom::combinator::map(nom::bytes::complete::tag("div"), |_| Ops::Div),
+        //eq
+        nom::combinator::map(nom::bytes::complete::tag("eq"), |_| Ops::Eq),
+        //lt
+        nom::combinator::map(nom::bytes::complete::tag("lt"), |_| Ops::Lt),
+        //SComb
+        nom::combinator::map(nom::bytes::complete::tag("s"), |_| Ops::SComb),
+        //CComb
+        nom::combinator::map(nom::bytes::complete::tag("c"), |_| Ops::CComb),
+        //BComb
+        nom::combinator::map(nom::bytes::complete::tag("b"), |_| Ops::BComb),
+        //TChoice
+        nom::combinator::map(nom::bytes::complete::tag("t"), |_| Ops::TChoice),
+        //FChoice
+        nom::combinator::map(nom::bytes::complete::tag("f"), |_| Ops::FChoice),
+        //IComb
+        nom::combinator::map(nom::bytes::complete::tag("i"), |_| Ops::IComb),
+        //Car
+        nom::combinator::map(nom::bytes::complete::tag("car"), |_| Ops::Car),
+        //Cdr
+        nom::combinator::map(nom::bytes::complete::tag("cdr"), |_| Ops::Cdr),
+        //IsNil
+        nom::combinator::map(nom::bytes::complete::tag("isnil"), |_| Ops::IsNil),
     ))(input)
 }
 
+fn parse_variable(input : &'_ str) -> IResult<&'_ str, Ops> {
+    nom::combinator::map(
+        nom::sequence::preceded(nom::character::complete::char(':'),nom::character::complete::alphanumeric1),
+        |label : &str| Ops::Variable(label.to_owned())
+    )(input)
+}
+
+fn parse_list(input : &'_ str) -> IResult<&'_ str, Ops> {
+    nom::combinator::map(
+        nom::sequence::tuple((
+            nom::bytes::complete::tag("( "),
+            nom::multi::separated_list(
+                nom::bytes::complete::tag(" , "),
+                parse_symbol
+            ),
+            nom::branch::alt((
+                nom::bytes::complete::tag(")"),
+                nom::bytes::complete::tag(" )")
+            ))
+        )),
+        | (_open_paren, symbols, _close_paren) | Ops::List(symbols)
+    ) (input)
+}
+
+
+
 pub fn parse_symbol(input : &'_ str) -> IResult<&'_ str, Ops> {
     nom::branch::alt((
+        parse_list,
         parse_literal,
+        parse_variable,
         parse_func
     ))(input)
 }
 
-pub fn _parse_line(input : &'_ str) -> IResult<&'_ str, Ops> {
-    //TODO handle the beginning of the line
-    parse_symbol(input)
+pub fn parse_line(input : &'_ str) -> IResult<&'_ str, (&'_ str, Ops)> {
+    nom::combinator::all_consuming(
+        nom::combinator::map(nom::sequence::tuple((
+            nom::sequence::preceded(nom::character::complete::char(':'), nom::character::complete::alphanumeric1),
+            nom::sequence::preceded(nom::character::complete::space1,nom::character::complete::char('=')),
+            nom::sequence::preceded(nom::character::complete::space1,parse_symbol)
+        )), |(label, _, op)| (label, op))
+    )(input)
 }
 
 #[cfg(test)]
@@ -132,9 +211,48 @@ mod tests {
         };
     }
     #[test]
-    fn test_parse_sym_ap_ap_cons_7_nil() {
-        let (remainder, op) = (parse_symbol("ap ap cons 7 nil")).unwrap();
+    fn test_parse_list_empty() {
+        let (remainder, op) = (parse_symbol("( )")).unwrap();
         assert_eq!(remainder, "");
-        dbg!(op);
+        match op {
+            Ops::List(x) => assert_eq!(x.len(), 0),
+            _ => assert!(false)
+        }
+    }
+    #[test]
+    fn test_parse_list_single() {
+        let (remainder, op) = (parse_symbol("( 1 )")).unwrap();
+        assert_eq!(remainder, "");
+        match op {
+            Ops::List(x) => assert_eq!(x.len(), 1),
+            _ => assert!(false)
+        }
+    }
+    #[test]
+    fn test_parse_list_single_op() {
+        let (remainder, op) = (parse_symbol("( nil )")).unwrap();
+        assert_eq!(remainder, "");
+        match op {
+            Ops::List(x) => assert_eq!(x.len(), 1),
+            _ => assert!(false)
+        }
+    }
+    #[test]
+    fn test_parse_list_multi() {
+        let (remainder, op) = (parse_symbol("( 1 , 2 , 3 )")).unwrap();
+        assert_eq!(remainder, "");
+        match op {
+            Ops::List(x) => assert_eq!(x.len(), 3),
+            _ => assert!(false)
+        }
+    }
+    #[test]
+    fn test_parse_list_multi_ops() {
+        let (remainder, op) = (parse_symbol("( ap inc 1 , 2 , 3 )")).unwrap();
+        assert_eq!(remainder, "");
+        match op {
+            Ops::List(x) => assert_eq!(x.len(), 3),
+            _ => assert!(false)
+        }
     }
 }
--- galaxy_parse/src/main.rs
+++ galaxy_parse/src/main.rs
@@ -2,5 +2,5 @@ mod galaxy_parse;
 use galaxy_parse::*;
 
 fn main() {
-    dbg!(parse_symbol("ap ap cons 8398848 ap ap cons 8407040 ap ap cons 8398849 ap ap cons 8407041 ap ap cons 8398850 ap ap cons 8402946 ap ap cons 8407042 ap ap cons 8398851 ap ap cons 8402947 ap ap cons 8407043 ap ap cons 8398852 ap ap cons 8402948 ap ap cons 8407044 ap ap cons 8390661 ap ap cons 8394757 ap ap cons 8398853 ap ap cons 8402949 ap ap cons 8407045 ap ap cons 8411141 ap ap cons 8415237 ap ap cons 8402950 nil"));
+    dbg!(parse_line(":1102 = ap ap cons :1097 ap ap cons :1098 ap ap cons :1099 ap ap cons :1100 nil"));
 }
