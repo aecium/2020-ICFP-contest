543b5f1b9ae8d82ac23d26f0c1966d29760edf1d
Nathan Frasier <nanoshot.nf@gmail.com>
2020-07-18 00:41:06 -0500

    Add some more parser combinator madness, for your reading pleasure

--- galaxy_parse/src/galaxy_parse.rs
+++ galaxy_parse/src/galaxy_parse.rs
@@ -14,7 +14,7 @@ pub enum Ops {
     */
     
     Ap(Box<Ops>, Box<Ops>),
-    Cons(Box<Ops>, Box<Ops>),
+    Cons,
     Nil,
 }
 
@@ -24,19 +24,69 @@ impl Ops {
             _ => Self::Nil
         }
     }
-    fn parse_literal(input : i128) -> Self {
-        Self::Nil
-    }
 }
 
-fn tokenize(input : &'_ str) -> IResult<&'_ str, &'_ str> {
+fn take_token(input : &'_ str) -> IResult<&'_ str, &'_ str> {
     nom::bytes::complete::is_not(" ")(input)
 }
 
-fn parse_token(input : &'_ str) -> IResult<&'_ str, Ops> {
-    Ok((input, Ops::Nil))
+fn parse_literal(input : &'_ str) -> IResult<&'_ str, Ops> {
+    nom::combinator::map_parser(nom::character::complete::digit1, |digits: &str| -> IResult<&'_ str, Ops>{
+        match input.parse::<i128>() {
+            Ok(num) => Ok((&input[input.len()..input.len()], Ops::Literal(num))),
+            _ => IResult::Err(nom::Err::Error(nom::error::make_error(input, nom::error::ErrorKind::IsNot)))
+        }
+    })(input)
+}
+
+fn parse_func(input : &'_ str) -> IResult<&'_ str, Ops> {
+    nom::combinator::map_parser(nom::character::complete::alpha1, |opName: &str| -> IResult<&'_ str, Ops>{
+        match opName {
+            "nil" => Ok((&input[input.len()..input.len()], Ops::Nil)),
+            "cons" => Ok((&input[input.len()..input.len()], Ops::Cons)),
+            "ap" => Ok((&input[input.len()..input.len()], Ops::Nil)), //TODO this is definitely wrong, time for recursion
+
+            _ => IResult::Err(nom::Err::Error(nom::error::make_error(input, nom::error::ErrorKind::IsNot)))
+        }
+    })(input)
 }
 
 pub fn parse_line(input : &'_ str) -> IResult<&'_ str, Ops> {
     Ok((input, Ops::Nil))
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn test_take_token() {
+        assert_eq!(take_token("42 "),Ok((" ", "42")))
+    }
+    #[test]
+    fn test_parse_literal() {
+        let (remainder, op) = (parse_literal("42")).unwrap();
+        assert_eq!(remainder, "");
+        match op {
+            Ops::Literal(42) => assert!(true),
+            _ => assert!(false)
+        };
+    }
+    #[test]
+    fn test_parse_func_nil() {
+        let (remainder, op) = (parse_func("nil")).unwrap();
+        assert_eq!(remainder, "");
+        match op {
+            Ops::Nil => assert!(true),
+            _ => assert!(false)
+        };
+    }
+    #[test]
+    fn test_parse_func_cons() {
+        let (remainder, op) = (parse_func("cons")).unwrap();
+        assert_eq!(remainder, "");
+        match op {
+            Ops::Cons => assert!(true),
+            _ => assert!(false)
+        };
+    }
 }
