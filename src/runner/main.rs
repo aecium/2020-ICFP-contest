7d1776c46a1f91717cd0ff5291e7d1e9da80bc75
jeorb <andy.meier@gmail.com>
2020-07-18 02:38:02 -0500

    Added variables to the parser.

--- src/runner/main.rs
+++ src/runner/main.rs
@@ -1,23 +1,27 @@
 use std::fs::File;
 use std::io::{self, BufRead};
 use std::path::Path;
-use std::collections::HashMap;
+use std::collections::BTreeMap;
 
-struct Var {
+struct Line {
+    num: usize,
+    key: String,
     src: String,
-    value: Vec<i64>,
+    symbols: Vec<Symbol>,
+    value: Vec<Symbol>,
+    has_value: bool,
 }
 
 fn main() {
-    let mut vars: HashMap<String, Var> = HashMap::new();
+    let mut lines: BTreeMap<String, Line> = BTreeMap::new();
 
-    if let Ok(lines) = read_lines("galaxy.txt") {
+    if let Ok(file_lines) = read_lines("galaxy.txt") {
         let mut i = 0;
-        for line in lines {
+
+        for line in file_lines {
             i+=1;
             if let Ok(l) = line {
-                let mut ap = 0;
-                println!("{}", l);
+                println!("Line {}: {}", i, l);
                 let mut sym_iter = l.split_whitespace();
                 let key = sym_iter.next().expect("Oops, no variable name.");
                 let _eq = sym_iter.next().expect("Oops, no equals.");
@@ -26,63 +30,135 @@ fn main() {
                 print!("{} = ", key);
                 for s in sym_iter {
                     let num = s.parse::<i64>();
-                    if s.eq("ap") {
-                        ap += 1;
-                        continue;
-                    } else if s.eq("cons") {
-                        symbols.push(Symbol::Cons);
-                    } else if s.eq("nil") {
-                        symbols.push(Symbol::Value(Vec::new()));
-                    } else if num.is_ok() {
+                    if num.is_ok() {
                         let mut v = Vec::new();
                         v.push(num.unwrap());
                         symbols.push(Symbol::Value(v));
+                    } else if s.eq("ap") {
+                        continue;
+                    } else if s.starts_with(":") {
+                        symbols.push(Symbol::Variable(s.to_string()));
+                    } else if s.eq("cons") {
+                        symbols.push(Symbol::Cons);
+                    } else if s.eq("neg") {
+                        symbols.push(Symbol::Negate);
+                    } else if s.eq("nil") {
+                        symbols.push(Symbol::Value(Vec::new()));
                     } else {
                         println!();
+                        println!("Line {}: Error parsing symbol.", i);
                         println!("TODO: {}", s);
+                        //panic!("At the disco!");
                     }
                     print!("{},", s);
                 }
                 println!();
 
-                let (value, _remaining) = eval(&vars, &symbols);
-                println!("key: {} - val: {:?} - ap: {}", key, value, ap);
-
-                vars.insert(key.to_string(), Var{src: l, value: value});
+                lines.insert(key.to_string(), Line {
+                    num: i,
+                    key: key.to_string(),
+                    src: l.to_string(),
+                    symbols: symbols,
+                    value: Vec::new(),
+                    has_value: false,
+                });
             }
-            if i > 13 {
+            if i > 200 {
                 break;
             }
         }
 
+        let keys: Vec<String> = lines.keys().cloned().collect();
+        for key in keys {
+            let symbols = lines.get(&key).expect("Unpossible.").symbols.clone();
+            let (value, _remaining) = eval(&mut lines, &symbols);
+            let line = lines.get_mut(&key).expect("Unpossible.");
+            println!("key: {} - val: {:?}", line.key, value);
+            line.value = value;
+        }
+
         println!();
         println!("Done!  Values:");
-        for var in vars.values() {
+        for var in lines.values() {
             println!("src: {}\n val: {:?}\n", var.src, var.value);
         }
     }
 }
 
-fn eval(vars: &HashMap<String, Var>, symbols: &[Symbol]) -> (Vec<i64>, Vec<Symbol>) {
-    let mut value: Vec<i64> = Vec::new();
+fn eval(vars: &mut BTreeMap<String, Line>, symbols: &[Symbol]) -> (Vec<Symbol>, Vec<Symbol>) {
+    let mut value: Vec<Symbol> = Vec::new();
     let mut symbols = symbols.to_vec();
     while symbols.len() > 0 {
         let s = symbols.remove(0);
-        match s {
-            Symbol::Value(val) => {
-                value = val.to_vec();
+        match s.clone() {
+            Symbol::Value(_val) => {
+                value.push(s);
+                //value = val.to_vec();
                 break;
             },
             Symbol::Cons => {
-                let (op1, remaining) = eval(&vars, &symbols);
-                let (op2, remaining) = eval(&vars, &remaining);
+                let (op1, remaining) = eval(vars, &symbols);
+                let (op2, remaining) = eval(vars, &remaining);
+                let mut ops: Vec<Symbol> = Vec::new();
+                ops.extend(op1);
+                ops.extend(op2);
                 let mut values: Vec<i64> = Vec::new();
-                values.extend(op1);
-                values.extend(op2);
+                for op in ops {
+                    match op {
+                        Symbol::Value(vec) => {
+                            values.extend(vec);
+                        },
+                        _ => {
+                            panic!("Awe, snap!");
+                        }
+                    }
+                    
+                }
                 symbols = remaining.to_vec();
                 symbols.push(Symbol::Value(values));
-
             },
+            Symbol::Negate => {
+                let (op1, remaining) = eval(vars, &symbols);
+                let mut ops: Vec<Symbol> = op1.to_vec();
+                let mut values: Vec<i64> = Vec::new();
+                /*let val1 = ops[0].clone();
+                match val1 {
+                    Symbol::Value(vec) => {
+                        vec[0] = -vec[0];
+                    },
+                    _ => {
+                        panic!("Awe, snap!");
+                    }
+                };*/
+                for op in ops {
+                    match op {
+                        Symbol::Value(vec) => {
+                            values.extend(vec);
+                        },
+                        _ => {
+                            panic!("Awe, snap!");
+                        }
+                    }
+                    
+                }
+                symbols = remaining.to_vec();
+                symbols.push(Symbol::Value(values));
+            },
+            Symbol::Variable(key) => {
+                let value: Vec<Symbol>;
+                println!("variable key: {}", key);
+                let mut symbols = vars.get(&key).expect("Oh, no symbols!").symbols.clone();
+                let has_value = vars.get(&key).expect("Oh, no has_value!").has_value;
+                if !has_value {
+                    let (op1, remaining) = eval(vars, &symbols);
+                    symbols.extend(op1);
+                } else {
+                    symbols.extend(vars.get(&key).expect("Oh, no!").value.clone());
+                }
+            }
+            _ => {
+                panic!("Please implement {:?}", s);
+            }
         };
     }
     (value, symbols)
@@ -99,5 +175,42 @@ where P: AsRef<Path>, {
 #[derive(Clone,Debug)]
 enum Symbol {
     Value(Vec<i64>),
+    Variable(String),
     Cons, //(or Pair)
+    Equality,
+    Successor,
+    Predecessor,
+    Sum,
+    Product,
+    IntegerDivision,
+    StrictLessThan,
+    Modulate,
+    Demodulate,
+    SendNow, //Send
+    Negate,
+    FuncApp, //Function Application
+    SC, //S Combinator
+    CC, //C Combinator
+    BC, //B Combinator
+    TC, // True (K Combinator)
+    FC, // False
+    PowerOfTwo,
+    IC, //i Combinator
+    Car, //(First)
+    Cdr, //(Tail)
+    Nil, //(Empty List)
+    IsNil, //(Is Empty List)
+    LCS, //List Construction Syntax
+    Vector,
+    Draw,
+    Checkerboard,
+    MulDraw, //Multiple Draw
+    ModList, //Modulate List
+    Send0,
+    Is0,
+    Interact,
+    InterProt, //Interaction Protocol
+    StatesessDraw, //Stateless Drawing Protocol
+    StatefullDraw, //Statefull Drawing Protocol
+    Galaxy,
 }
